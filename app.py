8fd6ca1dc51f5cd830fe00b5f680a72eec0fc514
Srija Bhattacharya <srija.bhattacharyya23@gmail.com>
2026-07-14 02:31:30 +0530

    Add AI Learning Buddy application using Streamlit

---
+++ app.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import google.generativeai as genai
+
+genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
+
+model = genai.GenerativeModel("gemini-2.5-flash")
+
+st.set_page_config(page_title="AI Learning Buddy Srija", page_icon="🎓")
+
+st.title("🎓 AI Learning Buddy Srija")
+
+topic = st.text_input("Enter a Topic")
+
+option = st.selectbox(
+    "Choose Activity",
+    [
+        "Explain Concept",
+        "Real-Life Example",
+        "Generate Quiz",
+        "Ask Anything"
+    ]
+)
+
+if st.button("Generate"):
+    if topic.strip() == "":
+        st.warning("Please enter a topic.")
+    else:
+        if option == "Explain Concept":
+            prompt = f"Explain {topic} in simple language for a beginner."
+        elif option == "Real-Life Example":
+            prompt = f"Give one simple real-life example of {topic}."
+        elif option == "Generate Quiz":
+            prompt = f"Create 5 MCQs on {topic} with answers."
+        else:
+            prompt = topic
+
+        response = model.generate_content(prompt)
+        st.write(response.text)
